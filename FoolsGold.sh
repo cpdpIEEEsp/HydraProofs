5f942b8363edc9152fbd040ab776ecbed786edbf
cpdpIEEEsp <152968578+cpdpIEEEsp@users.noreply.github.com>
2024-11-15 20:32:02 +0800

    Add files via upload

---
+++ FoolsGold.sh
@@ -0,0 +1,57 @@
+./build/src/main 10 14 1 1 2
+sleep 5
+./build/src/main 10 15 1 1 2
+sleep 5
+./build/src/main 10 16 1 1 2
+sleep 5
+./build/src/main 10 17 1 1 2
+sleep 5
+
+./build/src/main 7 17 1 1 2
+sleep 5
+./build/src/main 8 17 1 1 2
+sleep 5
+./build/src/main 9 17 1 1 2
+sleep 5
+./build/src/main 10 17 1 1 2
+sleep 5
+
+
+./build/src/main 10 14 2 1 2
+sleep 5
+./build/src/main 10 15 2 1 2
+sleep 5
+./build/src/main 10 16 2 1 2
+sleep 5
+./build/src/main 10 17 2 1 2
+sleep 5
+
+
+
+./build/src/main 7 17 2 1 2
+sleep 5
+./build/src/main 8 17 2 1 2
+sleep 5
+./build/src/main 9 17 2 1 2
+sleep 5
+./build/src/main 10 17 2 1 2
+sleep 5
+
+
+./build/src/main 10 14 1 64 2
+sleep 5
+./build/src/main 10 15 1 64 2
+sleep 5
+./build/src/main 10 16 1 64 2
+sleep 5
+./build/src/main 10 17 1 64 2
+sleep 5
+
+./build/src/main 10 14 2 64 2
+sleep 5
+./build/src/main 10 15 2 64 2
+sleep 5
+./build/src/main 10 16 2 64 2
+sleep 5
+./build/src/main 10 17 2 64 2
+sleep 5
